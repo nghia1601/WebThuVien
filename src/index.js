ce7464c77cb05be9c80adb95553e2d6066528045
nghia1601 <126093429+nghia1601@users.noreply.github.com>
2023-12-10 10:34:38 +0700

    tao PORT 3001 cho backend

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 const express = require("express");
-const 
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+    res.send('Hello World aaaaaaaaaa')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running in port: ', + PORT)
+})
+
